1efc30349198adc528abafa0e6e59743ba591c7c
gustavzudemm <lucas_97@gmx.net>
2023-10-22 08:57:23 +0200

    path fix

--- flask-introduction/website/__init__.py
+++ flask-introduction/website/__init__.py
@@ -24,6 +24,6 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path('website/' + DB_NAME):
+    if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
